e62382b699fd0fca675e047caa118e4bb0541975
Abhijith Reddy <areddy@voxware.com>
2013-09-13 18:53:41 -0400

    Api returns a map instead of a list.

--- src/com/archerabi/android/zillow/api/ZillowApi.java
+++ src/com/archerabi/android/zillow/api/ZillowApi.java
@@ -9,8 +9,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -43,16 +43,18 @@ public class ZillowApi {
 	}
 
 	/**
-	 * Starts a GET request to the demographics api and returns the results as a list of metrics.
-	 * This is a synchronous method.
+	 * Starts a GET request to the demographics api and returns the results as a
+	 * map of metrics. This is a synchronous method.
+	 * 
 	 * @param state
 	 * @param cityName
 	 * @return
 	 */
-	public List<Metric> getDemographicInformation(String state, String cityName) {
+	public Map<String,Metric> getDemographicInformation(String state, String cityName) {
 		String uri = null;
 		try {
 			uri = ZILLOW_API_HOST + String.format(GET_DEMOGRAPHICS_URI, mApiKey, state) + URLEncoder.encode(cityName, "UTF-8");
+			Log.d(getClass().getName(), "URI = "+uri);
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
@@ -65,8 +67,9 @@ public class ZillowApi {
 		try {
 			request.setURI(new URI(uri));
 			response = client.execute(request);
-			List<Metric> metrics = parseDemographics(response.getEntity().getContent());
+			Map<String,Metric> metrics = parseDemographics(response.getEntity().getContent());
 			Log.d(getClass().getName(), "Metric List size is " + metrics.size());
+			return metrics;
 		} catch (URISyntaxException e1) {
 			e1.printStackTrace();
 		} catch (ClientProtocolException e) {
@@ -79,13 +82,14 @@ public class ZillowApi {
 
 	/**
 	 * Parse the results from getDemographicInformation
+	 * 
 	 * @param in
 	 * @return
 	 * @throws IOException
 	 */
-	private List<Metric> parseDemographics(InputStream in) throws IOException {
+	private Map<String, Metric> parseDemographics(InputStream in) throws IOException {
 		XmlPullParser parser = Xml.newPullParser();
-		List<Metric> entries = new ArrayList<Metric>();
+		Map<String, Metric> entries = new HashMap<String, Metric>();
 		try {
 			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
 			parser.setInput(in, null);
@@ -95,7 +99,8 @@ public class ZillowApi {
 				String name = parser.getName();
 				if (name != null && name.compareTo("attribute") == 0) {
 					parser.next();
-					entries.add(parseAttribute(parser));
+					Metric metric = parseAttribute(parser);
+					entries.put(metric.getName(), metric);
 				}
 			}
 			return entries;
@@ -112,9 +117,10 @@ public class ZillowApi {
 		return null;
 	}
 
-
 	/**
-	 * Parse {@code attribute} tags. Since each attribute is modeled as a {@link Metric} , this method returns a Metric object.
+	 * Parse {@code attribute} tags. Since each attribute is modeled as a
+	 * {@link Metric} , this method returns a Metric object.
+	 * 
 	 * @param parser
 	 * @return
 	 * @throws XmlPullParserException
@@ -130,7 +136,9 @@ public class ZillowApi {
 			if (parser.getName().compareTo("city") == 0) {
 				parser.next();
 				try {
-					metric.setCityValue(Float.parseFloat(readText(parser, "value")));
+					float value = Float.parseFloat(readText(parser, "value"));
+					metric.setCityValue(value);
+					metric.setValue(value);
 				} catch (NumberFormatException e) {
 				}
 				parser.next();
@@ -153,12 +161,14 @@ public class ZillowApi {
 	}
 
 	/**
-	 * Reads text correspoding to {@code tag} and moves the parser to the next tag. <br/>
+	 * Reads text correspoding to {@code tag} and moves the parser to the next
+	 * tag. <br/>
 	 * For Example <br/>
 	 * <br/>
 	 * The following xml will be successfully parsed <br>
-	 * {@code <name>Zillow Home Value Index</name>}.
-	 * <br>The string "Zillow Home Value Index" will be returned.
+	 * {@code <name>Zillow Home Value Index</name>}. <br>
+	 * The string "Zillow Home Value Index" will be returned.
+	 * 
 	 * @param parser
 	 * @param tag
 	 * @return
